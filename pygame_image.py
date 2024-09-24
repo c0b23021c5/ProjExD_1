fc66c913ed480d8acd956b9727add31762b77739
C0B23021 <c0b23021c5@edu.teu.ac.jp>
2024-09-24 16:24:27 +0900

    こいつ...うごくぞ！？

--- pygame_image.py
+++ pygame_image.py
@@ -23,15 +23,28 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            ca_rct.move_ip((0,-1))
+        if key_lst[pg.K_DOWN]:
+            ca_rct.move_ip((0,+1))
+        if key_lst[pg.K_LEFT]:
+            ca_rct.move_ip((-1,0))
+        if key_lst[pg.K_RIGHT]:
+            ca_rct.move_ip((+1,0))   
         x = -(tmr%3200)
         screen.blit(bg_img, [x, 0])
         screen.blit(bg_img2, [x+1600, 0])
         screen.blit(bg_img, [x+3200, 0])
         screen.blit(bg_img2, [x+4800, 0])
         screen.blit(ca_img,ca_rct)
+
         # txt = font.render(str(tmr), True, (255, 255, 255))
         # screen.blit(txt, [300, 100])
-
+        # txt = font.render(str(ca_rct[1]), True, (255, 255, 255))
+        # screen.blit(txt, [300, 100])
+        # print(ca_rct[1])
+        
         pg.display.update()
         tmr += 1
         clock.tick(200)
