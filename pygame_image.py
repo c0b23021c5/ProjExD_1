75c6576a597302ad482a61eb8d3f226c408b4c62
C0B23021 <c0b23021c5@edu.teu.ac.jp>
2024-09-24 16:13:34 +0900

    コウカトンrect

--- pygame_image.py
+++ pygame_image.py
@@ -10,15 +10,28 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img2,True,False)
     ca_img = pg.image.load("fig/3.png")           #コウカトン召喚
     ca_img = pg.transform.flip(ca_img,True,False)  #transform　引数１：画像　引数２：左右変更　引数３：上下変更
+    ca_rct = ca_img.get_rect()
+    ca_rct.center = 300,200
     tmr = 0
+
+    # font = pg.font.Font(None, 80)
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = -(tmr%800)
+        x = -(tmr%3200)
         screen.blit(bg_img, [x, 0])
-        screen.blit(ca_img,[300,200])
+        screen.blit(bg_img2, [x+1600, 0])
+        screen.blit(bg_img, [x+3200, 0])
+        screen.blit(bg_img2, [x+4800, 0])
+        screen.blit(ca_img,ca_rct)
+        # txt = font.render(str(tmr), True, (255, 255, 255))
+        # screen.blit(txt, [300, 100])
+
         pg.display.update()
         tmr += 1
         clock.tick(200)
